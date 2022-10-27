5b51938d1a50527aea0aec7854017f8907fcd4f6
Daishuai <867390427@qq.com>
2022-10-27 23:51:06 +0800

    tomcat

--- server-dept/src/main/java/com/daishuai/dept/DeptApplication.java
+++ server-dept/src/main/java/com/daishuai/dept/DeptApplication.java
@@ -2,6 +2,9 @@ package com.daishuai.dept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -12,9 +15,15 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @EnableEurekaClient
 @SpringBootApplication
-public class DeptApplication {
+@ServletComponentScan
+public class DeptApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DeptApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DeptApplication.class);
     }
 }
